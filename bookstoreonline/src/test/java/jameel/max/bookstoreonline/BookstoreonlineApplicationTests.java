5ad90ff4fff714e71ef7e058ff05102c558148ef
Zain <Zain@Zain-PC.home>
2020-05-17 15:53:07 -0500

    just create github repository and clone it and run spring project inside it

---
+++ bookstoreonline/src/test/java/jameel/max/bookstoreonline/BookstoreonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package jameel.max.bookstoreonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
